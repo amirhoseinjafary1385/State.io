67e5f620712c55bd3581662ec12494dad69f7ad0
Amir hossein jafary <114409072+amirhoseinjafary1385@users.noreply.github.com>
2024-08-10 19:17:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_USER'] = 'your_username'
+app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'your_password_name'
 app.config['MYSQL_DB'] = 'stateio' 
 app.config['MYSQL_HOST'] = '127.0.0.1' #localhost
